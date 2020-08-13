aa443729452122bd75ab777732235be3d63955af
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-08-12 21:42:44 -0500

    initializing spring boot project

---
+++ src/test/java/dev/gerardo/shipping_app/ShippingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gerardo.shipping_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
