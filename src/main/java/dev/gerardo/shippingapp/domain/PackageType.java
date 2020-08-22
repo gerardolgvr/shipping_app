e23abffb50c45ffadc1466ae93fc0d5e8e6e37cd
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-08-22 17:37:47 -0500

    adding lombok library

--- src/main/java/dev/gerardo/shippingapp/domain/PackageType.java
+++ src/main/java/dev/gerardo/shippingapp/domain/PackageType.java
@@ -1,40 +1,16 @@
 package dev.gerardo.shippingapp.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PackageType {
 
     private int id;
     private String description;
     private float price;
 
-    public PackageType(){ }
-
-    public PackageType(int id, String description, float price) {
-        this.id = id;
-        this.description = description;
-        this.price = price;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
 }
