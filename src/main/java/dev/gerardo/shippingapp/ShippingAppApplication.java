1435976a2631570403bc48b3ab62e85df00b4c16
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-09-01 11:56:31 -0500

    enabling cors to application level

--- src/main/java/dev/gerardo/shippingapp/ShippingAppApplication.java
+++ src/main/java/dev/gerardo/shippingapp/ShippingAppApplication.java
@@ -2,12 +2,27 @@ package dev.gerardo.shippingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShippingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShippingAppApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/type")
+						.allowedMethods("GET")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
--- src/main/java/dev/gerardo/shippingapp/controller/PackageTypeController.java
+++ src/main/java/dev/gerardo/shippingapp/controller/PackageTypeController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,7 +15,6 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin
 @RestController
 public class PackageTypeController {
 
