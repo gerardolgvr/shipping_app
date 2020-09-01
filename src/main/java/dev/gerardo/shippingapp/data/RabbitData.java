ce9d28bfeaa5d87447d74e707ac889bc91624e5e
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-08-31 23:09:25 -0500

    adding constants, separating request and receive for general purpose

---
+++ src/main/java/dev/gerardo/shippingapp/data/RabbitData.java
@@ -0,0 +1,23 @@
+package dev.gerardo.shippingapp.data;
+
+import dev.gerardo.shippingapp.config.RabbitMQConfigProperties;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitData {
+
+    private final AmqpTemplate rabbitTemplate;
+    private final RabbitMQConfigProperties rabbitMQConfigProperties;
+
+    public RabbitData(AmqpTemplate rabbitTemplate, RabbitMQConfigProperties rabbitMQConfigProperties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.rabbitMQConfigProperties = rabbitMQConfigProperties;
+    }
+
+    public Object getData(String request){
+        return rabbitTemplate.convertSendAndReceive(rabbitMQConfigProperties.getExchange(),
+                rabbitMQConfigProperties.getRoutingKey(),
+                request);
+    }
+}
