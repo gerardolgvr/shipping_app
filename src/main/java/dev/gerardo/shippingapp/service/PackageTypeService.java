e065f4bf9eb5d772874dd5ebad74fdcf286d9b24
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-08-31 23:11:57 -0500

    updating service layer and their corresponding tests

--- src/main/java/dev/gerardo/shippingapp/service/PackageTypeService.java
+++ src/main/java/dev/gerardo/shippingapp/service/PackageTypeService.java
@@ -1,16 +1,17 @@
 package dev.gerardo.shippingapp.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.gerardo.shippingapp.config.RabbitMQConfigProperties;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import dev.gerardo.shippingapp.constants.RabbitMQConstants;
+import dev.gerardo.shippingapp.data.RabbitData;
 import dev.gerardo.shippingapp.domain.PackageType;
 import dev.gerardo.shippingapp.exception.UnavailableServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -18,23 +19,16 @@ public class PackageTypeService {
 
     private static final Logger logger = LoggerFactory.getLogger(PackageTypeService.class);
 
-    private final AmqpTemplate rabbitTemplate;
-    private final RabbitMQConfigProperties rabbitMQConfigProperties;
+    private final RabbitData rabbitData;
     private final ObjectMapper mapper;
 
-    public PackageTypeService(AmqpTemplate rabbitTemplate,
-                              RabbitMQConfigProperties rabbitMQConfigProperties,
-                              ObjectMapper mapper) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.rabbitMQConfigProperties = rabbitMQConfigProperties;
+    public PackageTypeService(RabbitData rabbitData, ObjectMapper mapper) {
+        this.rabbitData = rabbitData;
         this.mapper = mapper;
     }
 
     public List<PackageType> getPackageTypes() throws JsonProcessingException {
-        String request = "{\"type\":\"packageType\"}";
-        String response = String.valueOf(rabbitTemplate.convertSendAndReceive(rabbitMQConfigProperties.getExchange(),
-                rabbitMQConfigProperties.getRoutingKey(),
-                request));
+        String response = String.valueOf(rabbitData.getData(RabbitMQConstants.PACKAGE_TYPE_REQUEST));
 
         if (response.equals("null")) {
             logger.error("An error ocurred trying to parse: {}", response);
@@ -44,8 +38,7 @@ public class PackageTypeService {
     }
 
     public List<PackageType> parseToPackageTypes(String json) throws JsonProcessingException {
-        return mapper.readValue(json, new TypeReference<List<PackageType>>() {
-        });
+        return Arrays.asList(mapper.readValue(json, PackageType[].class));
     }
 
 }
--- src/test/java/dev/gerardo/shippingapp/service/PackageTypeServiceTest.java
+++ src/test/java/dev/gerardo/shippingapp/service/PackageTypeServiceTest.java
@@ -1,20 +1,18 @@
 package dev.gerardo.shippingapp.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.gerardo.shippingapp.config.RabbitMQConfigProperties;
+import dev.gerardo.shippingapp.constants.RabbitMQConstants;
+import dev.gerardo.shippingapp.data.RabbitData;
 import dev.gerardo.shippingapp.domain.PackageType;
 import dev.gerardo.shippingapp.exception.UnavailableServiceException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.amqp.core.AmqpTemplate;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,7 +26,7 @@ public class PackageTypeServiceTest {
     @InjectMocks
     PackageTypeService packageTypeService;
     @Mock
-    private AmqpTemplate rabbitTemplate;
+    private RabbitData rabbitData;
     @Mock
     private RabbitMQConfigProperties rabbitMQConfigProperties;
     @Mock
@@ -36,17 +34,15 @@ public class PackageTypeServiceTest {
 
     @Test
     public void shouldGetPackageTypes() throws JsonProcessingException {
+
         // Given:
         String json = "[{\"id\":3,\"description\":\"Envelop\",\"price\":5},{\"id\":4,\"description\":\"Box\",\"price\":10}]";
-        String request = "{\"type\":\"packageType\"}";
-        when(rabbitTemplate.convertSendAndReceive(
-                rabbitMQConfigProperties.getExchange(),
-                rabbitMQConfigProperties.getRoutingKey(),
-                request)).thenReturn(json);
-        List<PackageType> packageTypesList = new LinkedList<>();
-        packageTypesList.add(new PackageType(3, "Envelop", 5f));
-        packageTypesList.add(new PackageType(4, "Box", 10f));
-        when(objectMapper.readValue(eq(json), Mockito.<TypeReference<List<PackageType>>>any())).thenReturn(packageTypesList);
+        when(rabbitData.getData(RabbitMQConstants.PACKAGE_TYPE_REQUEST)).thenReturn(json);
+        PackageType[] packageTypesList = new PackageType[]{
+                new PackageType(3, "Envelop", 5f),
+                new PackageType(4, "Box", 10f)
+        };
+        when(objectMapper.readValue(eq(json), eq(PackageType[].class))).thenReturn(packageTypesList);
 
         // When:
         List<PackageType> packageTypes = packageTypeService.getPackageTypes();
@@ -56,34 +52,35 @@ public class PackageTypeServiceTest {
         assertThat(packageTypes.get(0).getDescription()).isEqualTo("Envelop");
         assertThat(packageTypes.get(1).getId()).isEqualTo(4);
         assertThat(packageTypes.get(1).getDescription()).isEqualTo("Box");
+
     }
 
     @Test
     public void shouldParseJsonToPackageTypes() throws JsonProcessingException {
+
         // Given:
-        List<PackageType> packageTypesList = new LinkedList<>();
-        packageTypesList.add(new PackageType(3, "Envelop", 5f));
-        packageTypesList.add(new PackageType(4, "Box", 10f));
+        PackageType[] packageTypesList = new PackageType[]{
+                new PackageType(3, "Envelop", 5f),
+                new PackageType(4, "Box", 10f)
+        };
         String json = "[{\"id\":3,\"description\":\"Envelop\",\"price\":5},{\"id\":4,\"description\":\"Box\",\"price\":10}]";
-        when(objectMapper.readValue(eq(json), Mockito.<TypeReference<List<PackageType>>>any())).thenReturn(packageTypesList);
+        when(objectMapper.readValue(eq(json), eq(PackageType[].class))).thenReturn(packageTypesList);
 
         // When:
         List<PackageType> packageTypes = packageTypeService.parseToPackageTypes(json);
 
         // Then:
-        assertThat(packageTypesList.get(0).getDescription()).isEqualTo("Envelop");
-        assertThat(packageTypesList.get(1).getDescription()).isEqualTo("Box");
+        assertThat(packageTypesList[0].getDescription()).isEqualTo("Envelop");
+        assertThat(packageTypesList[1].getDescription()).isEqualTo("Box");
+
     }
 
     @Test
     public void testServiceWhenDataIsNotAvailable() {
+
         // Given:
         String json = "null";
-        String request = "{\"type\":\"packageType\"}";
-        when(rabbitTemplate.convertSendAndReceive(
-                rabbitMQConfigProperties.getExchange(),
-                rabbitMQConfigProperties.getRoutingKey(),
-                request)).thenReturn(json);
+        when(rabbitData.getData(RabbitMQConstants.PACKAGE_TYPE_REQUEST)).thenReturn(json);
 
         // When:
         Throwable thrown = catchThrowable(() -> {
