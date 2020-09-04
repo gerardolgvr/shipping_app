5df90050481e9a406e1cb2689a3a77c31d3833ba
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-09-04 00:38:50 -0500

    separating cors to a configuration file

--- src/main/java/dev/gerardo/shippingapp/ShippingAppApplication.java
+++ src/main/java/dev/gerardo/shippingapp/ShippingAppApplication.java
@@ -13,16 +13,4 @@ public class ShippingAppApplication {
 		SpringApplication.run(ShippingAppApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/type")
-						.allowedMethods("GET")
-						.allowedOrigins("*");
-			}
-		};
-	}
-
 }
